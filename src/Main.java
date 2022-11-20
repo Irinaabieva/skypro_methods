7627a3798520f9d7ce31a053427a050009d6f4a1
Irinaabieva <irinaabieva03@gmail.com>
2022-11-20 20:34:37 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Main {
     // Задание 1
     public static void isLeap(int year) {
@@ -7,7 +9,26 @@ public class Main {
             System.out.println(year + " - невисокосный год");
         }
     }
+    // Задание 2
+    public static void suggestInstall(int clientOs, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        if (clientOs == 0) {
+            if (clientDeviceYear >= currentYear) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        }   else {
+            if (clientDeviceYear >= currentYear) {
+                System.out.println("Установите  версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         isLeap(2023);
+        suggestInstall(0,2022);
     }
 }
