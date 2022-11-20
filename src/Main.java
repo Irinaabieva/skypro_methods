a05b0d7f1fc2a60d2e99380027bc5bef3bd6a0bc
Irinaabieva <irinaabieva03@gmail.com>
2022-11-20 20:16:51 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
 public class Main {
+    // Задание 1
+    public static void isLeap(int year) {
+        if (year % 4 == 0) {
+            System.out.println(year + " - Високосный год");
+        } else {
+            System.out.println(year + " - невисокосный год");
+        }
+    }
     public static void main(String[] args) {
+        isLeap(2023);
     }
 }
